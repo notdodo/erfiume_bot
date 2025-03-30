4490d12671df4fae61585cb8d9b7cb39404ce112
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2025-03-30 14:29:36 +0200

    enh: respond to supergroup's topics (#153)

--- app/bot/src/commands/utils.rs
+++ app/bot/src/commands/utils.rs
@@ -1,4 +1,10 @@
-pub(crate) fn escape_markdown_v2(text: &str) -> String {
+use teloxide::{
+    payloads::SendMessageSetters,
+    prelude::{Bot, Requester},
+    types::{LinkPreviewOptions, Message, ParseMode},
+};
+
+fn escape_markdown_v2(text: &str) -> String {
     text.replace("\\", "\\\\")
         .replace("_", "\\_")
         .replace("*", "\\*")
@@ -17,4 +23,24 @@ pub(crate) fn escape_markdown_v2(text: &str) -> String {
         .replace("}", "\\}")
         .replace(".", "\\.")
         .replace("!", "\\!")
+}
+
+pub(crate) async fn send_message(
+    bot: &Bot,
+    msg: &Message,
+    preview_options: LinkPreviewOptions,
+    text: &str,
+) -> Result<teloxide::prelude::Message, teloxide::RequestError> {
+    if let Some(thread_id) = msg.thread_id {
+        bot.send_message(msg.chat.id, escape_markdown_v2(text))
+            .link_preview_options(preview_options)
+            .message_thread_id(thread_id)
+            .parse_mode(ParseMode::MarkdownV2)
+            .await
+    } else {
+        bot.send_message(msg.chat.id, escape_markdown_v2(text))
+            .link_preview_options(preview_options)
+            .parse_mode(ParseMode::MarkdownV2)
+            .await
+    }
 }
