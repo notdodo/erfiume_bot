0f27175a81e873890b25d84ba13d1926b441114c
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2025-08-10 16:18:17 +0200

    enh: code mgmt

--- app/fetcher/src/main.rs
+++ app/fetcher/src/main.rs
@@ -2,121 +2,20 @@ use anyhow::Result;
 use aws_config::BehaviorVersion;
 use aws_sdk_dynamodb::Client as AWSClient;
 use dynamodb::DynamoDbClient;
-use futures::StreamExt;
 use lambda_runtime::{Error as LambdaError, LambdaEvent, service_fn};
 use reqwest::Client as HTTPClient;
-use serde_json::{Value, json};
-use station::{Entry, Station, StationData};
+use serde_json::Value;
 use std::error::Error as StdError;
 use std::time::Duration;
-use tracing::{error, instrument};
+use tracing::instrument;
 use tracing_subscriber::EnvFilter;
+
+use crate::region::Region;
 mod dynamodb;
 mod region;
 mod station;
 
 type BoxError = Box<dyn StdError + Send + Sync>;
-
-async fn fetch_latest_time(client: &HTTPClient) -> Result<i64, BoxError> {
-    let response = client
-        .get("https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time?variabile=254,0,0/1,-,-,-/B13215&time=1726667100000")
-        .send()
-        .await?;
-
-    response.error_for_status_ref()?;
-
-    let entries: Vec<Entry> = response.json().await?;
-    for entry in entries {
-        if let Entry::TimeEntry { time } = entry {
-            let timestamp = time
-                .parse::<i64>()
-                .map_err(|e| format!("Failed to parse 'time': {e}"))?;
-            return Ok(timestamp);
-        }
-    }
-
-    Err("No 'TimeEntry' found in response".into())
-}
-
-async fn fetch_stations(client: &HTTPClient, timestamp: i64) -> Result<Vec<Station>, BoxError> {
-    let url = format!(
-        "https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time?variabile=254,0,0/1,-,-,-/B13215&time={timestamp}"
-    );
-    let response = client.get(&url).send().await?;
-    response.error_for_status_ref()?;
-
-    let entries: Vec<Entry> = response.json().await?;
-    let stations = entries
-        .into_iter()
-        .filter_map(|e| match e {
-            Entry::DataEntry {
-                idstazione,
-                ordinamento,
-                nomestaz,
-                lon,
-                soglia1,
-                value: _,
-                soglia2,
-                lat,
-                soglia3,
-                timestamp: _,
-            } => Some(Station {
-                idstazione,
-                ordinamento,
-                nomestaz,
-                lon,
-                soglia1,
-                soglia2,
-                soglia3,
-                lat,
-                timestamp: None,
-                value: None,
-            }),
-            Entry::TimeEntry { .. } => None,
-        })
-        .collect();
-    Ok(stations)
-}
-
-async fn fetch_station_data(
-    client: &HTTPClient,
-    mut station: Station,
-) -> Result<Station, BoxError> {
-    let url = format!(
-        "https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-time-series/?stazione={}&variabile=254,0,0/1,-,-,-/B13215",
-        station.idstazione
-    );
-    let response = client.get(&url).send().await?;
-    response.error_for_status_ref()?;
-    let entries: Vec<StationData> = response.json().await?;
-    if let Some(latest_value) = entries.iter().max_by_key(|e| e.t) {
-        station.timestamp = Some(latest_value.t);
-        station.value = latest_value.v;
-    }
-
-    Ok(station)
-}
-
-async fn process_station(
-    client: &HTTPClient,
-    dynamodb_client: &DynamoDbClient,
-    station: Station,
-) -> Result<(), BoxError> {
-    let station = fetch_station_data(client, station.clone())
-        .await
-        .map_err(|e| {
-            error!(
-                "Error fetching data for station {}: {:?}",
-                station.nomestaz, e
-            );
-            e
-        });
-    dynamodb_client
-        .put_station_into_dynamodb(&station?, "EmiliaRomagna-Stations")
-        .await?;
-
-    Ok(())
-}
 
 #[instrument(skip(dynamodb_client))]
 async fn lambda_handler(
@@ -124,40 +23,10 @@ async fn lambda_handler(
     dynamodb_client: &DynamoDbClient,
     _: LambdaEvent<Value>,
 ) -> Result<Value, LambdaError> {
-    let latest_timestamp = fetch_latest_time(http_client).await?;
-    let stations = fetch_stations(http_client, latest_timestamp).await?;
-    let concurrency_limit = 40;
-
-    let process_futures = stations
-        .clone()
-        .into_iter()
-        .map(|station| process_station(http_client, dynamodb_client, station));
-
-    let process_results: Vec<_> = futures::stream::iter(process_futures)
-        .buffer_unordered(concurrency_limit)
-        .collect()
-        .await;
-
-    let successful_updates = process_results.iter().filter(|res| res.is_ok()).count();
-    let mut error_count = 0;
-    for result in process_results {
-        if let Err(e) = result {
-            error_count += 1;
-            error!(error = %e, "Error processing station: {:?}", e);
-        }
-    }
-    if error_count > 0 {
-        error!(%error_count, "Aborting Lambda execution due to errors");
-        return Err(anyhow::anyhow!("Processing failed for {} stations", error_count).into());
-    }
-
-    Ok(json!({
-        "message": "Lambda executed successfully",
-        "stations_found": stations.len(),
-        "stations_updated": successful_updates,
-        "errors": error_count,
-        "statusCode": 200,
-    }))
+    let results = region::emilia_romagna::EmiliaRomagna
+        .fetch_stations_data(http_client, dynamodb_client)
+        .await?;
+    Ok(serde_json::to_value(results)?)
 }
 
 #[tokio::main]
--- app/fetcher/src/region/mod.rs
+++ app/fetcher/src/region/mod.rs
@@ -1,16 +1,24 @@
-use crate::{BoxError, dynamodb, station::Station};
+use crate::dynamodb;
 use dynamodb::DynamoDbClient;
 use reqwest::Client as HTTPClient;
 pub mod emilia_romagna;
+use lambda_runtime::Error as LambdaError;
+use serde::{Deserialize, Serialize};
 
-#[allow(dead_code)]
 pub trait Region {
-    async fn name(&self) -> &'static str;
-    async fn dynamodb_table(&self) -> &'static str;
-
+    fn dynamodb_table(&self) -> &'static str;
     async fn fetch_stations_data(
         &self,
         http_client: &HTTPClient,
         dynamodb_client: &DynamoDbClient,
-    ) -> Result<Station, BoxError>;
+    ) -> Result<RegionResult, LambdaError>;
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RegionResult {
+    message: String,
+    stations_found: usize,
+    stations_updated: usize,
+    errors: i64,
+    status_code: i64,
 }
